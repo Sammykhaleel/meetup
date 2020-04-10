e1844c451639cad3fc301123c09542eaa577ec36
sammykhaleel <sammykhaleel83@gmail.com>
2020-04-10 15:05:17 -0400

    pushing

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -2,22 +2,50 @@
 
 const axios = require("axios");
 
-module.exports.getAccessToken = async event => {
+module.exports.getAccessToken = async (event) => {
   const MEETUP_OAUTH_URL =
     "https://secure.meetup.com/oauth2/access" +
     "?client_id=6lrpip852j7ksr4u06d69bbbdj" +
     "&client_secret=jeimv3h67cqid77pk723ss840i" +
     "&grant_type=authorization_code" +
     "&redirect_uri=https://sammykhaleel.github.io/meetup/" +
-    "&code=99935756b21d9f4468ec7b54339faff9";
+    "&code=" +
+    event.pathParameters.code;
 
   const info = await axios.post(MEETUP_OAUTH_URL);
 
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
     body: JSON.stringify({
       access_token: info.data.access_token,
-      refresh_token: info.data.refresh_token
-    })
+      refresh_token: info.data.refresh_token,
+    }),
+  };
+};
+
+module.exports.refreshAccessToken = async (event) => {
+  const MEETUP_OAUTH_URL =
+    "https://secure.meetup.com/oauth2/access" +
+    "?client_id=6lrpip852j7ksr4u06d69bbbdj" +
+    "&client_secret=jeimv3h67cqid77pk723ss840i" +
+    "&grant_type=authorization_code" +
+    "&redirect_uri=https://sammykhaleel.github.io/meetup/" +
+    "&code=" +
+    event.pathParameters.code;
+
+  const info = await axios.post(MEETUP_OAUTH_URL);
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      access_token: info.data.access_token,
+      refresh_token: info.data.refresh_token,
+    }),
   };
 };
